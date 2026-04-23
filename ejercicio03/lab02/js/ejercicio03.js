83c64618588286d8239d26e2d10dad1af4b19742
Valeria TN <vticonan@unsa.edu.pe>
2026-04-23 01:22:27 -0500

    Actualizando codigos

--- ejercicio03/lab02/js/ejercicio03.js
+++ ejercicio03/lab02/js/ejercicio03.js
@@ -5,19 +5,15 @@ let progreso = [];
 let errores = 0;
 let usadas = [];
 
-// CONTENEDOR
 const contenedor = document.createElement("div");
 contenedor.className = "contenedor";
 
-// TITULO
 const titulo = document.createElement("h1");
 titulo.innerText = "El Ahorcado - Arequipa";
 
-// SECCION DEl JUEGO
 const juego = document.createElement("div");
 juego.className = "juego";
 
-// CANVAS
 const canvasBox = document.createElement("div");
 canvasBox.className = "canvas-box";
 
@@ -28,7 +24,6 @@ const ctx = canvas.getContext("2d");
 
 canvasBox.appendChild(canvas);
 
-// INFORMACION de letras, ganadas y errores
 const info = document.createElement("div");
 info.className = "info";
 
@@ -42,32 +37,20 @@ const erroresDiv = document.createElement("div");
 erroresDiv.className = "errores";
 
 const mensaje = document.createElement("div");
+mensaje.className = "mensaje";
 
 const boton = document.createElement("button");
 boton.className = "boton";
 boton.innerText = "Iniciar Juego";
 
-// LETRAS
 const letrasDiv = document.createElement("div");
 letrasDiv.className = "letras";
 
-// ARMAR
-info.appendChild(texto);
-info.appendChild(palabraDiv);
-info.appendChild(erroresDiv);
-info.appendChild(mensaje);
-info.appendChild(boton);
-
-juego.appendChild(canvasBox);
-juego.appendChild(info);
-
-contenedor.appendChild(titulo);
-contenedor.appendChild(juego);
-contenedor.appendChild(letrasDiv);
-
+info.append(texto, palabraDiv, erroresDiv, mensaje, boton);
+juego.append(canvasBox, info);
+contenedor.append(titulo, juego, letrasDiv);
 document.body.appendChild(contenedor);
 
-// INICIAR
 boton.onclick = iniciarJuego;
 
 function iniciarJuego() {
@@ -76,6 +59,9 @@ function iniciarJuego() {
     errores = 0;
     usadas = [];
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath(); // 🔥 FIX IMPORTANTE
+
     palabraDiv.innerText = progreso.join(" ");
     erroresDiv.innerHTML = "";
     mensaje.innerHTML = "";
@@ -83,12 +69,9 @@ function iniciarJuego() {
 
     boton.innerText = "Volver a jugar";
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     generarLetras();
 }
 
-// LETRAS
 function generarLetras() {
     for (let l of "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
         const btn = document.createElement("button");
@@ -123,13 +106,11 @@ function generarLetras() {
                 usadas.filter(x => !palabra.includes(x)).join(", ");
 
             if (!progreso.includes("_")) {
-                mensaje.className = "mensaje ganar";
-                mensaje.innerHTML = "¡Ganaste! <br> La palabra era: " + palabra;
+                mensaje.innerHTML = "<div class='ganar'>¡Ganaste!<br>La palabra era: " + palabra + "</div>";
             }
 
             if (errores === 10) {
-                mensaje.className = "mensaje perder";
-                mensaje.innerHTML = "Perdiste <br> Era: " + palabra;
+                mensaje.innerHTML = "<div class='perder'>Perdiste<br>Era: " + palabra + "</div>";
             }
         };
 
@@ -137,18 +118,21 @@ function generarLetras() {
     }
 }
 
-// CANVAS
 function dibujar(p) {
+    ctx.beginPath();
+
     switch (p) {
-        case 1: ctx.moveTo(10,290); ctx.lineTo(200,290); ctx.stroke(); break;
-        case 2: ctx.moveTo(50,290); ctx.lineTo(50,50); ctx.stroke(); break;
-        case 3: ctx.moveTo(50,50); ctx.lineTo(150,50); ctx.stroke(); break;
-        case 4: ctx.moveTo(150,50); ctx.lineTo(150,80); ctx.stroke(); break;
-        case 5: ctx.beginPath(); ctx.arc(150,100,20,0,Math.PI*2); ctx.stroke(); break;
-        case 6: ctx.moveTo(150,120); ctx.lineTo(150,200); ctx.stroke(); break;
-        case 7: ctx.moveTo(150,140); ctx.lineTo(120,170); ctx.stroke(); break;
-        case 8: ctx.moveTo(150,140); ctx.lineTo(180,170); ctx.stroke(); break;
-        case 9: ctx.moveTo(150,200); ctx.lineTo(120,240); ctx.stroke(); break;
-        case 10: ctx.moveTo(150,200); ctx.lineTo(180,240); ctx.stroke(); break;
+        case 1: ctx.moveTo(10, 290); ctx.lineTo(200, 290); break;
+        case 2: ctx.moveTo(50, 290); ctx.lineTo(50, 50); break;
+        case 3: ctx.moveTo(50, 50); ctx.lineTo(150, 50); break;
+        case 4: ctx.moveTo(150, 50); ctx.lineTo(150, 80); break;
+        case 5: ctx.arc(150, 100, 20, 0, Math.PI * 2); break;
+        case 6: ctx.moveTo(150, 120); ctx.lineTo(150, 200); break;
+        case 7: ctx.moveTo(150, 140); ctx.lineTo(120, 170); break;
+        case 8: ctx.moveTo(150, 140); ctx.lineTo(180, 170); break;
+        case 9: ctx.moveTo(150, 200); ctx.lineTo(120, 240); break;
+        case 10: ctx.moveTo(150, 200); ctx.lineTo(180, 240); break;
     }
+
+    ctx.stroke();
 }
